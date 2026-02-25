0bc514427696fb040ec6ab4cecfb666a10b6b3d3
Aluno <Aluno@fatecdiadema.com>
2026-02-25 12:32:17 -0300

    feat: adicao do projeto

---
+++ app/src/main/java/br/gov/sp/cps/buttontext/MainActivity.java
@@ -0,0 +1,40 @@
+package br.gov.sp.cps.buttontext;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import java.util.Random;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+    public void sorteiaNumero(View view){
+        //vincula o elemento textResultado (xml) com o Java:
+        TextView textResultado = findViewById(R.id.textResultado);
+
+        //lógica de números aleatórios:
+        int num = new Random().nextInt(1, 101);
+
+        //alterar texto:
+        textResultado.setText("n" + num);
+
+    }
+
+}
